1f0ef46cff2d33a1445802b09949468d5d4d7af8
lukybil <lukybil@gmail.com>
2022-06-24 14:05:27 +0200

    united download section items size with a grid

--- next/components/atoms/DownloadItem.tsx
+++ next/components/atoms/DownloadItem.tsx
@@ -54,24 +54,26 @@ export const DownloadItem = ({ downloadItem, oneLine = false, className = '' }: 
   }
 
   return (
-    <div>
+    <div className="flex h-full flex-col justify-between">
       <h4 className="pb-8 text-lg lg:pb-10 3xl:pb-12">{downloadItem.title ?? file?.name ?? ''}</h4>
-      <div className="relative left-[-10px] h-fit w-fit">
-        <FileIcon width={100} fill="#fff" className="relative" />
-        <span className="absolute top-1/2 left-1/2 block -translate-x-1/2 -translate-y-1/2 uppercase">
-          {file?.ext?.slice(1, 5)}
-        </span>
+      <div>
+        <div className="relative left-[-10px] h-fit w-fit">
+          <FileIcon fill="#fff" className="relative w-24" />
+          <span className="absolute top-1/2 left-1/2 block -translate-x-1/2 -translate-y-1/2 uppercase">
+            {file?.ext?.slice(1, 5)}
+          </span>
+        </div>
+        <Button
+          disabled={fetching}
+          size="link"
+          color="light"
+          onClick={() => download(file?.url, file?.name)}
+          aria-label="download file"
+          className="pt-8 lg:pt-12"
+        >
+          {t('common.download')}
+        </Button>
       </div>
-      <Button
-        disabled={fetching}
-        size="link"
-        color="light"
-        onClick={() => download(file?.url, file?.name)}
-        aria-label="download file"
-        className="pt-8 lg:pt-12"
-      >
-        {t('common.download')}
-      </Button>
     </div>
   )
 }
--- next/components/molecules/sections/DownloadSection.tsx
+++ next/components/molecules/sections/DownloadSection.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 import { DownloadItemFragment } from '../../../graphql'
 import { isDefined } from '../../../utils/isDefined'
 import DownloadItem from '../../atoms/DownloadItem'
@@ -19,7 +17,12 @@ const DownloadSection = ({ files, title, anchor }: DownloadSectionProps) => {
   return (
     <Section anchor={anchor} color="dark" className="px-xStandard py-yHigh">
       <h2 className="pb-12 text-xxl lg:pb-44">{title}</h2>
-      <div className="lg:gap-x-auto flex flex-wrap justify-start gap-x-14 lg:flex-nowrap lg:justify-start lg:gap-y-9">
+      {/* <div className="lg:gap-x-auto flex flex-wrap justify-start gap-x-14 lg:flex-nowrap lg:justify-start lg:gap-y-9">
+        {files?.filter(isDefined).map((item) => (
+          <DownloadItem downloadItem={item} key={item.id} />
+        ))}
+        </div> */}
+      <div className="grid grid-flow-row grid-cols-1 gap-x-xStandard gap-y-yStandard md:grid-cols-3 lg:grid-cols-5">
         {files?.filter(isDefined).map((item) => (
           <DownloadItem downloadItem={item} key={item.id} />
         ))}
