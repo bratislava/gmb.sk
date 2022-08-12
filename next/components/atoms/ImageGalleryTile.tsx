bde3e11d49dc80d190c25eb8b2ebb8506562f23f
lukybil <81419334+lukybil@users.noreply.github.com>
2022-08-12 15:51:08 +0200

    feature/GMB-323 Gallery section redesign (#57)
    * recreated image gallery according to the marianum design
    
    * changed md breakpoint to sm
    
    * fixed gallery component
    
    * Update ImageGallery.tsx
    
    Co-authored-by: Richi <generator@mydevelops.com>
    Co-authored-by: Richi <richard.dvorsky@bratislava.sk>

--- next/components/atoms/ImageGalleryTile.tsx
+++ next/components/atoms/ImageGalleryTile.tsx
@@ -1,7 +1,6 @@
 import cx from 'classnames'
 
 import { ImageWithFormatsEntityFragment } from '../../graphql'
-import { StrapiImageFormats } from '../../types/strapiImageFormats'
 import { WithAttributes } from '../../utils/isDefined'
 
 interface ImageGalleryTileProps {
@@ -14,11 +13,10 @@ interface ImageGalleryTileProps {
 const ImageGalleryTile = ({ image, className, onChoose, index }: ImageGalleryTileProps) => {
   if (!image) return null
 
-  const { url, formats, alternativeText } = image.attributes
-  const src = index === 0 ? url : (formats as StrapiImageFormats).thumbnail.url ?? url
+  const { url, alternativeText } = image.attributes
   return (
     <button type="button" className={cx(className, 'h-full w-full')} onClick={() => onChoose(index)}>
-      <img src={src} alt={alternativeText ?? ''} className="h-full w-full object-cover" />
+      <img src={url} alt={alternativeText ?? ''} className="h-full w-full object-cover" />
     </button>
   )
 }
