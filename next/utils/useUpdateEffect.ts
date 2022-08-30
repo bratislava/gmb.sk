d6b4033da94ead059038cee3525feaec7c3c49c7
Július Retzer <jul.retzer@gmail.com>
2022-08-30 14:59:21 +0200

    Track page view on load

---
+++ next/utils/useUpdateEffect.ts
@@ -0,0 +1,18 @@
+import { useEffect, useRef } from "react";
+
+/**
+ * A custom useEffect hook that only triggers on updates, not on initial mount
+ */
+ export default function useUpdateEffect(effect: () => void, dependencies: unknown[] = []) {
+  const isInitialMount = useRef(true);
+
+  // eslint-disable-next-line consistent-return
+  useEffect(() => {
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
+    } else {
+      return effect();
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, dependencies);
+}
