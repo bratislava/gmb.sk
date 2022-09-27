e7318bd1f5275742166f26a5bd331a64e25f1d83
Július Retzer <jul.retzer@gmail.com>
2022-09-27 12:41:29 +0200

    Reduce sample rate

--- next/sentry.server.config.js
+++ next/sentry.server.config.js
@@ -8,5 +8,5 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
   dsn: SENTRY_DSN || 'https://6054a49808e144c182fab778e6a33c72@o701870.ingest.sentry.io/6703546',
-  tracesSampleRate: 1.0,
-});
+  tracesSampleRate: 0.2,
+})
