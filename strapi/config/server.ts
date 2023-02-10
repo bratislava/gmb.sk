c71d31cac0b56d9ab7fb4cf0d76184e91f5e6c19
Radoslav Zeman <radoslav.zeman@bratislava.sk>
2023-02-10 13:16:56 +0100

    try dev strapi url

--- strapi/config/server.ts
+++ strapi/config/server.ts
@@ -1,7 +1,8 @@
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  port: env.int("PORT", 1338),
   app: {
     keys: env.array("APP_KEYS"),
   },
+  url: "https://city-gallery-strapi.dev.bratislava.sk/",
 });
