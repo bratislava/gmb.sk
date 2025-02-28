0a8ba2738a4e9bf154fe08df4a094fc6bf37a3fb
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2025-02-28 15:33:48 +0100

    Temporarily remove custom Preview Button plugin

--- strapi/src/plugins/preview-button-i18n/admin/src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Initializer } from './Initializer';
-export { default as Injector } from './Injector';
-export { default as PreviewButton } from './PreviewButton';
--- strapi/src/plugins/preview-button-i18n/server/utils/get-service.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const pluginId = require( './plugin-id' );
-
-const getService = name => {
-  return strapi.plugin( pluginId ).service( name );
-};
-
-module.exports = getService;
