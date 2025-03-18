71975833a3ed036ce5e8493882e0b4d01d831e27
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2025-03-18 14:46:53 +0100

    Add admin custom translations

--- strapi/src/admin/app.tsx
+++ strapi/src/admin/app.tsx
@@ -10,6 +10,34 @@ export default {
     notifications: {
       releases: false,
     },
+
+    translations: {
+      sk: {
+        "content-manager.components.LeftMenu.collection-types": "Kolekcie",
+        "content-manager.components.LeftMenu.single-types": "Jednoduché typy",
+        "content-manager.containers.Edit.information.lastUpdate":
+          "Aktualizoané",
+        "modal.upload-list.sub-header.add-folder": "Pridať priečinok",
+        "app.components.LeftMenu.general": "Všeobecné",
+        "Auth.form.username.placeholder": "napr. jankohrasko",
+        "Auth.form.email.placeholder": "napr. janko.hrasko@bratislava.sk",
+        "Settings.webhooks.trigger.test": "Testovací beh",
+        // Used in page.contentState field
+        "contentState.todo": "TODO",
+        "contentState.inProgress": "IN PROGRESS",
+        "contentState.finalising": "FINALISING",
+        "contentState.inReview": "IN REVIEW",
+        "contentState.done": "DONE",
+      },
+      en: {
+        // Used in page.contentState field
+        "contentState.todo": "TODO",
+        "contentState.inProgress": "IN PROGRESS",
+        "contentState.finalising": "FINALISING",
+        "contentState.inReview": "IN REVIEW",
+        "contentState.done": "DONE",
+      },
+    },
   },
   bootstrap() {},
 };
