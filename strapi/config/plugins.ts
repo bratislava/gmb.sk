c71dd40989b0a7b0c9ea4a0bf4ec35c7e79fccea
Jana Běhal Dadová <jana.dadova@bratislava.sk>
2025-03-26 00:03:21 +0100

    Setup email provider

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -1,11 +1,26 @@
-import meilisearchConfig from "./plugins.meilisearch.config";
-import graphqlConfig from "./plugins.graphql.config";
+import meilisearchConfig from "./plugins.meilisearch.config"
+import graphqlConfig from "./plugins.graphql.config"
 
-export default {
+module.exports = ({ env }) => ({
   graphql: {
     config: graphqlConfig,
   },
   meilisearch: {
     config: meilisearchConfig,
   },
-};
+  email: {
+    config: {
+      //  mailgun email provider https://www.npmjs.com/package/@strapi/provider-email-mailgun
+      provider: 'mailgun',
+      providerOptions: {
+        key: env('MAILGUN_API_KEY'), // Required
+        domain: env('MAILGUN_DOMAIN'), // Required
+        url: 'https://api.eu.mailgun.net', //Optional. If domain region is Europe use 'https://api.eu.mailgun.net'
+      },
+      settings: {
+        defaultFrom: env('MAILGUN_EMAIL'),
+        defaultReplyTo: env('MAILGUN_EMAIL')
+      },
+    },
+  },
+})
