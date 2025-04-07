56817c398b02cfa945b26e80eced94ddc2b22aad
Katarína Zápražná <katarina.zaprazna@bratislava.sk>
2025-04-07 12:40:48 +0200

    Make non-breaking changes to the AppLangSwitchers component for later expansion

--- next/src/components/atoms/AppLangSwitchers.tsx
+++ next/src/components/atoms/AppLangSwitchers.tsx
@@ -1,7 +1,7 @@
 import cx from 'classnames'
 import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next'
-import { useCallback } from 'react'
+import React, { useCallback } from 'react'
 
 import { ContentPageEntityFragment } from '@/src/services/graphql'
 import { WithAttributes } from '@/src/utils/isDefined'
@@ -13,66 +13,59 @@ interface AppLangSwitchersProps {
 }
 
 const AppLangSwitchers = ({ contentPage, showBothLanguages }: AppLangSwitchersProps) => {
+  const { t } = useTranslation()
   const router = useRouter()
-  const { i18n } = useTranslation()
-  const currentLanguage = i18n.language
+
+  const { pathname, locale: currentLocale, locales } = router // Use router to access locale for better Next integration
 
   const onLocaleChange = useCallback(
     (locale: string) => {
       const equivalentRouteInTargetLocale = getEquivalentRouteInTargetLocale(
-        router.pathname,
+        pathname,
         locale,
         contentPage
       )
 
       if (!equivalentRouteInTargetLocale) {
         // eslint-disable-next-line @typescript-eslint/no-floating-promises
-        router.push(
-          {
-            pathname: `/404`,
-          },
-          undefined,
-          { locale }
-        )
+        router.push({ pathname: `/404` }, undefined, { locale })
 
         return
       }
 
       // eslint-disable-next-line @typescript-eslint/no-floating-promises
-      router.replace(
-        {
-          pathname: equivalentRouteInTargetLocale,
-        },
-        undefined,
-        { locale }
-      )
+      router.replace({ pathname: equivalentRouteInTargetLocale }, undefined, { locale })
     },
-    [router, contentPage]
+    [pathname, contentPage, router]
   )
 
   return (
-    <div className={cx('flex', { 'text-nav': !showBothLanguages })}>
-      <button
-        className={cx('cursor-pointer', {
-          'font-semibold': currentLanguage === 'sk',
-          hidden: currentLanguage === 'sk' && !showBothLanguages,
-        })}
-        onClick={() => onLocaleChange('sk')}
-        type="button"
-      >
-        SK
-      </button>
-      {showBothLanguages && <div className="px-2">/</div>}
-      <button
-        className={cx('cursor-pointer', {
-          'font-semibold': currentLanguage === 'en',
-          hidden: currentLanguage === 'en' && !showBothLanguages,
-        })}
-        onClick={() => onLocaleChange('en')}
-        type="button"
-      >
-        EN
-      </button>
+    <div className={cx('flex', showBothLanguages ? 'items-center space-x-2' : 'text-nav')}>
+      {locales?.map((locale, index) => {
+        const shouldShowLocale = showBothLanguages || locale !== currentLocale
+        const showSlash = showBothLanguages && index === 0
+
+        return (
+          <React.Fragment key={locale}>
+            {shouldShowLocale ? (
+              <button
+                type="button"
+                onClick={() => onLocaleChange(locale)}
+                aria-label={
+                  locale === 'sk' ? t('localization.aria.slovak') : t('localization.aria.english')
+                }
+                className={cx('cursor-pointer uppercase', {
+                  'font-semibold': currentLocale === locale,
+                })}
+              >
+                {locale}
+              </button>
+            ) : null}
+
+            {showSlash ? <span aria-hidden>/</span> : null}
+          </React.Fragment>
+        )
+      })}
     </div>
   )
 }
