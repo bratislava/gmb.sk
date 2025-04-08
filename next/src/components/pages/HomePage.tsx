131c95bdbc94707da883392123f355e78e1ab8df
Katarína Zápražná <katarina.zaprazna@bratislava.sk>
2025-04-08 16:26:24 +0200

    Rename Page component to HomePage

--- next/src/components/pages/HomePage.tsx
+++ next/src/components/pages/HomePage.tsx
@@ -2,41 +2,24 @@ import { useTranslation } from 'next-i18next'
 
 import SeoHead from '@/src/components/atoms/SeoHead'
 import HighlightsSection from '@/src/components/molecules/sections/HighlightsSection'
-import MapSection from '@/src/components/molecules/sections/MapSection'
 import NewsletterSection from '@/src/components/molecules/sections/NewsletterSection'
 import NewsSection from '@/src/components/molecules/sections/NewsSection'
 import OpeningHoursSection from '@/src/components/molecules/sections/OpeningHoursSection'
 import PageSectionContainer from '@/src/components/molecules/sections/PageSectionContainer'
 import PartnersSection from '@/src/components/molecules/sections/PartnersSection'
-import RichtextSection from '@/src/components/molecules/sections/RichtextSection'
-import TicketsSection from '@/src/components/molecules/sections/TicketsSection'
 import Submenu from '@/src/components/molecules/Submenu'
 import PageWrapper from '@/src/components/pages/PageWrapper'
-import {
-  AboutUsPageQuery,
-  CollectionPageQuery,
-  GetInvolvedPageQuery,
-  HomePageQuery,
-  NewsItemEntityFragment,
-  TicketEntityFragment,
-  VisitUsPageQuery,
-} from '@/src/services/graphql'
+import { HomePageQuery, NewsItemEntityFragment } from '@/src/services/graphql'
 import { getAnchor } from '@/src/utils/getAnchor'
 import { hasAttributes, isDefined, WithAttributes } from '@/src/utils/isDefined'
 
-interface PageProps {
-  page:
-    | AboutUsPageQuery['aboutUsPage']
-    | GetInvolvedPageQuery['getInvolvedPage']
-    | VisitUsPageQuery['visitUsPage']
-    | CollectionPageQuery['collectionsPage']
-    | HomePageQuery['homePage']
+interface HomePageProps {
+  page: HomePageQuery['homePage']
   title: string
   newsItems?: WithAttributes<NewsItemEntityFragment>[] | null
-  tickets?: WithAttributes<TicketEntityFragment>[] | null
 }
 
-const Page = ({ page: pageResponse, title, newsItems, tickets }: PageProps) => {
+const HomePage = ({ page: pageResponse, title, newsItems }: HomePageProps) => {
   const { t } = useTranslation()
 
   const page = pageResponse?.data?.attributes
@@ -107,44 +90,10 @@ const Page = ({ page: pageResponse, title, newsItems, tickets }: PageProps) => {
             )
           }
 
-          if (section.__typename === 'ComponentSectionsMapSection') {
-            return (
-              <MapSection
-                title={section.title ?? undefined}
-                anchor={getAnchor(section.submenuTitle)}
-                key={`${section.__typename}-${section.id}`}
-              />
-            )
-          }
-
-          if (section.__typename === 'ComponentSectionsTicketsSection' && tickets) {
-            return (
-              <TicketsSection
-                title={section.title ?? undefined}
-                text={section.text ?? undefined}
-                anchor={getAnchor(section.submenuTitle)}
-                tickets={tickets}
-                key={`${section.__typename}-${section.id}`}
-              />
-            )
-          }
-
-          if (section.__typename === 'ComponentSectionsRichtextSection') {
-            return (
-              <RichtextSection
-                content={section.content}
-                anchor={getAnchor(section.submenuTitle)}
-                className="px-xMd py-yMd"
-                key={`${section.__typename}-${section.id}`}
-              />
-            )
-          }
-
           return null
         })}
 
-      {pageResponse?.data?.attributes?.__typename === 'HomePage' &&
-      pageResponse.data?.attributes?.partners?.length ? (
+      {pageResponse?.data?.attributes?.partners?.length ? (
         <PartnersSection
           title={t('common.partners')}
           partners={pageResponse.data?.attributes?.partners
@@ -156,4 +105,4 @@ const Page = ({ page: pageResponse, title, newsItems, tickets }: PageProps) => {
   )
 }
 
-export default Page
+export default HomePage
