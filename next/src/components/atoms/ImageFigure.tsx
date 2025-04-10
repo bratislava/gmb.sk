1665aa16b248d82c68dbb0a5fe8443962ff5322c
jankaBehal <jana.dadova@bratislava.sk>
2025-04-10 16:49:52 +0200

    Fix caption in patch @strapi/admin (#363)
    * Fix caption in patch @strapi/admin
    
    * Remove noCaption from ImageFigure
    
    * Exchange || for ??

--- next/src/components/atoms/ImageFigure.tsx
+++ next/src/components/atoms/ImageFigure.tsx
@@ -1,18 +1,12 @@
-interface IImageProps extends React.HTMLProps<HTMLImageElement> {
-  noCaption?: boolean
-}
+interface IImageProps extends React.HTMLProps<HTMLImageElement> {}
 
-const ImageFigure = ({ src, alt, noCaption, width, height, sizes }: IImageProps) => {
+const ImageFigure = ({ src, alt, width, height, sizes }: IImageProps) => {
   const [altText, caption] = alt?.includes('||') ? alt?.split('||') || ['', ''] : [alt, alt]
 
   return (
     <figure className="relative max-w-full">
       <img src={src} alt={altText} width={width} height={height} sizes={sizes} />
-      {!noCaption && alt && (
-        <figcaption aria-hidden={caption === alt} className="my-yMd block text-sm text-gmbGray">
-          {caption || alt}
-        </figcaption>
-      )}
+      {caption && <figcaption className="my-yMd block text-sm text-gmbGray">{caption}</figcaption>}
     </figure>
   )
 }
