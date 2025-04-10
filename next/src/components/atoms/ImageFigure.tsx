226817c1fd27dad22eeff2cf91ac679e27d8f466
Jana Běhal Dadová <jana.dadova@bratislava.sk>
2025-04-10 13:15:26 +0200

    Fix caption in patch @strapi/admin

--- next/src/components/atoms/ImageFigure.tsx
+++ next/src/components/atoms/ImageFigure.tsx
@@ -8,9 +8,9 @@ const ImageFigure = ({ src, alt, noCaption, width, height, sizes }: IImageProps)
   return (
     <figure className="relative max-w-full">
       <img src={src} alt={altText} width={width} height={height} sizes={sizes} />
-      {!noCaption && alt && (
+      {(caption || (!noCaption && altText)) && (
         <figcaption aria-hidden={caption === alt} className="my-yMd block text-sm text-gmbGray">
-          {caption || alt}
+          {caption || altText}
         </figcaption>
       )}
     </figure>
