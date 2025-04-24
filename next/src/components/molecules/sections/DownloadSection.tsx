9f148b6e51d7f333f029651ac4ed0ab57ab98bbd
Katarína Zápražná <katarina.zaprazna@bratislava.sk>
2025-04-24 12:49:04 +0200

    #389 Adjust padding for title in DownloadSection component (#393)
    Adjust padding for title in DownloadSection component

--- next/src/components/molecules/sections/DownloadSection.tsx
+++ next/src/components/molecules/sections/DownloadSection.tsx
@@ -16,7 +16,7 @@ const DownloadSection = ({ files, title, anchor }: DownloadSectionProps) => {
 
   return (
     <Section anchor={anchor} color="dark" className="px-xMd py-yLg">
-      {title && <h2 className="pb-yXl text-xxl">{title}</h2>}
+      {title && <h2 className="pb-yLg text-xxl">{title}</h2>}
       <div className="grid grid-flow-row grid-cols-2 gap-x-xMd gap-y-yMd md:grid-cols-3 lg:grid-cols-5">
         {files?.filter(isDefined).map((item) => <DownloadItem downloadItem={item} key={item.id} />)}
       </div>
