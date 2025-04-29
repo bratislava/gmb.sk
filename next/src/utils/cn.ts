38fa972175a94be459af98c59ab3fa283e2eabad
Katarína Zápražná <katarina.zaprazna@bratislava.sk>
2025-04-29 22:04:35 +0200

    Extend twMerge function to register custom values

--- next/src/utils/cn.ts
+++ next/src/utils/cn.ts
@@ -1,10 +1,21 @@
 import clsx, { type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { extendTailwindMerge } from 'tailwind-merge'
+
+import tailwindConfig from '@/tailwind.config'
 
 /**
  * Based on OLO: https://github.com/bratislava/olo.sk/blob/master/next/src/utils/cn.ts
  * Inspired by: https://ui.shadcn.com/docs/installation/manual
  */
+
+const twMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      'font-size': Object.keys(tailwindConfig?.theme?.fontSize ?? {}).map((key) => `text-${key}`),
+      'font-weight': ['font-regular', 'font-heavy'], // Include custom font weights
+    },
+  },
+})
 
 const cn = (...args: ClassValue[]) => {
   return twMerge(clsx(...args))
