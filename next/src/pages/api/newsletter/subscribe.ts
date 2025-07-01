9d323d2f16514691ee706ea99f8c1b18d5ed2ec5
Adam <jakubicka.adam@gmail.com>
2025-07-01 13:49:04 +0200

    Remove client error when subscribing an already subscribed mail

--- next/src/pages/api/newsletter/subscribe.ts
+++ next/src/pages/api/newsletter/subscribe.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import mailchimp from '@mailchimp/mailchimp_marketing'
 import { NextApiRequest, NextApiResponse } from 'next'
 
@@ -5,6 +6,10 @@ mailchimp.setConfig({
   apiKey: process.env.MAILCHIMP_API_KEY,
   server: process.env.MAILCHIMP_API_SERVER,
 })
+
+/**
+ * Based on City library: https://github.com/bratislava/mestskakniznica.sk/blob/master/next/pages/api/subscribe.ts
+ */
 
 const Subscribe = async (req: NextApiRequest, res: NextApiResponse) => {
   const { email } = req.body
@@ -24,8 +29,17 @@ const Subscribe = async (req: NextApiRequest, res: NextApiResponse) => {
     }
     throw new Error('Invalid audience.')
   } catch (error: any) {
-    console.log(`error ${error.toString()}`)
-    // TODO: duplicate email gives error 500
+    // If the e-mail is already subscribed, mailchimp throws an error
+    // However we handle it as success so we don't confuse the user
+    const isEmailAlreadySubscribed = /member exists/i.test(error?.response?.text)
+    if (isEmailAlreadySubscribed) {
+      console.log('Email was already subscribed before')
+
+      return res.status(201).json({ error: '' })
+    }
+
+    console.log('Newsletter subscription error:')
+    console.log(error.message ?? JSON.stringify(error))
 
     return res.status(500).json({ error: error.message || error.toString() })
   }
