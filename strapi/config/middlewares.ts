31cae696cfb9d5ffa4a6b0c730ba5ddad523af6d
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2025-09-02 09:16:59 +0200

    #467 Setup aws upload provider for s3 minio

--- strapi/config/middlewares.ts
+++ strapi/config/middlewares.ts
@@ -1,29 +1,42 @@
-export default [
-  "strapi::logger",
-  "strapi::errors",
+export default ({ env }) => [
+  'strapi::logger',
+  'strapi::errors',
   /**
    * Setting up strapi::security middleware to avoid some blocking (e.g. thumbnails in Media Library are not shown without this setting).
    * Followed readme of Google Cloud provider package, because I couldn't find it in documentation of strapi nor minio provider package.
    * https://github.com/strapi-community/strapi-provider-upload-google-cloud-storage/blob/master/README.md#setting-up-strapisecurity-middlewares-to-avoid-csp-blocked-url
    */
   {
-    name: "strapi::security",
+    name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
         directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": ["'self'", "data:", "blob:", "cdn-api.bratislava.sk"],
-          "media-src": ["'self'", "data:", "blob:", "cdn-api.bratislava.sk"],
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'cdn-api.bratislava.sk',
+            `${env('MINIO_BUCKET')}.s3.bratislava.sk`,
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'cdn-api.bratislava.sk',
+            `${env('MINIO_BUCKET')}.s3.bratislava.sk`,
+          ],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
-];
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+]
