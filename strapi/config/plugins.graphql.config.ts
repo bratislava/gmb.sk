a148612e80993aed1375096525dd30459bfbc2c2
shegood <petrsigut10@gmail.com>
2026-07-16 13:48:52 +0200

    put v4 compatibilityon and update packagelock

--- strapi/config/plugins.graphql.config.ts
+++ strapi/config/plugins.graphql.config.ts
@@ -5,6 +5,7 @@ const graphqlConfig = {
     // When changing schema path, also change watchIgnoreFiles in strapi/config/admin.js
     schema: true,
   },
-};
+  v4CompatibilityMode: true,
+}
 
-export default graphqlConfig;
+export default graphqlConfig
