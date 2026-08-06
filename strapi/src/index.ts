6e7c507e7e2f7bac90769496bcd90e346c8a476c
shegood <petrsigut10@gmail.com>
2026-08-06 10:19:26 +0200

    return back the fixed webhook management

--- strapi/src/index.ts
+++ strapi/src/index.ts
@@ -10,8 +10,7 @@ export default {
    * This gives you an opportunity to extend code.
    */
 
-  register({ strapi }: { strapi: Core.Strapi }) {
-  },
+  register({ strapi }: { strapi: Core.Strapi }) {},
 
   /**
    * An asynchronous bootstrap function that runs before
@@ -20,6 +19,47 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  async bootstrap({ strapi }: { strapi: Core.Strapi }) {
+  async bootstrap({ strapi }) {
+    //------------------------------------
+    // ADDING REVALIDATE WEBHOOK
+    //------------------------------------
+    // create Revalidate webhook according to this suggestion https://github.com/strapi/strapi/pull/20487#issuecomment-2482527848
+    const webhooks = await strapi.get('webhookStore').findWebhooks()
+    const webhook = webhooks.find((w) => w.name === 'Bootstrapped Revalidate')
+
+    if (!webhook) {
+      await strapi.get('webhookStore').createWebhook({
+        id: 'Bootstrapped Revalidate',
+        name: 'Bootstrapped Revalidate',
+        url: `${process.env.REVALIDATE_NEXT_URL}/api/revalidate?secret=${process.env.REVALIDATE_SECRET_TOKEN}`,
+        events: ['entry.create', 'entry.update', 'entry.publish'],
+        headers: {},
+        isEnabled: true,
+      })
+      console.log('Revalidate webhook created')
+    } else {
+      console.log('Revalidate webhook already exists')
+    }
+    //------------------------------------
+    // ADDING ENGLISH LOCALE
+    //------------------------------------
+    const existingEnglish = await strapi.db
+      .query('plugin::i18n.locale')
+      .findOne({ where: { code: 'en' } })
+    if (!existingEnglish) {
+      const english = { name: 'English (en)', code: 'en' }
+      try {
+        await strapi.db.query('plugin::i18n.locale').create({ data: english })
+      } catch (error: any) {
+        console.log('Caught error while creating locale, checking if locale created successfully.')
+        const createdEnglish = await strapi.db
+          .query('plugin::i18n.locale')
+          .findOne({ where: english })
+        if (createdEnglish) console.log('Created English locale.')
+      }
+    }
+    console.log({
+      locales: await strapi.db.query('plugin::i18n.locale').findMany(),
+    })
   },
 }
